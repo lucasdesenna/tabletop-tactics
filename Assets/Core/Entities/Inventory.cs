e1220f1b86242ce2560cf67c13fc733df58e7219
Lucas <lucasdesenna@gmail.com>
2022-12-09 01:03:57 -0300

    Add basic Inventory features

---
+++ Assets/Core/Entities/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Inventory
+{
+    public readonly int? Capacity = null;
+
+    public Inventory()
+    {
+
+    }
+
+    public Inventory(int itemCapacity)
+    {
+        Capacity = itemCapacity;
+    }
+
+    public List<Item> Items = new();
+
+    public bool IsFull => Capacity != null && Items.Count() == Capacity;
+
+    public void AddItem(Item item)
+    {
+        if (IsFull) { throw new InvalidOperationException(); }
+
+        Items.Add(item);
+    }
+
+    public void RemoveItem(Item item) => Items.Remove(item);
+}
