b498f28e307ff42d76c401e26557a2147a51c4a8
Lucas <lucasdesenna@gmail.com>
2022-12-10 02:59:43 -0300

    Add Trinket Entity

---
+++ Assets/Core/Entities/Trinket.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Core.Enums;
+
+namespace Assets.Core.Entities
+{
+    public class Trinket : Item
+    {
+        public readonly TrinketType TrinketType;
+
+        public Trinket(string name, string description, TrinketType trinketType) 
+            : base(name, description, ItemType.TRINKET)
+        {
+            TrinketType = trinketType;
+        }
+    }
+}
---
+++ Assets/Core/Enums/TrinketType.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Core.Enums
+{
+    public enum TrinketType
+    {
+        STAFF_OF_POWER_1,
+        STAFF_OF_POWER_2, 
+        STAFF_OF_POWER_3,
+        RING_OF_POWER_1,
+        ORB_OF_POWER_1,
+        STAFF_OF_CASTING,
+        BOOTS_OF_SPEED,
+        RING_OF_SLOW_FALL,
+        RING_OF_WILL,
+        RING_OF_TELEPORTATION,
+        GLOVES_OF_STRENGTH,
+        ROBES_OF_ARROW_TURNING,
+        AMULET_OF_RESITANCE,
+        CANDLE_OF_SUMMONING,
+        GLOVES_OF_CASTING,
+        BELT_OF_ANIMAL_REPELLENCE,
+        HORN_OF_DESTRUCTION,
+        FATE_STONE,
+        DRINKING_HORN_OF_HEALING,
+        BANNER_OF_COURAGE
+    }
+}
