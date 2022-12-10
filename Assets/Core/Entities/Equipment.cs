160f9ac245e7257327d0f1ac7606d185d820da17
Lucas <lucasdesenna@gmail.com>
2022-12-10 02:56:41 -0300

    Add Equipment Entity

---
+++ Assets/Core/Entities/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Core.Enums;
+
+namespace Assets.Core.Entities
+{
+    public class Equipment : Item
+    {
+        public readonly EquipmentType EquipmentType;
+
+        public Equipment(string name, string description, EquipmentType equipmentType)
+            : base(name, description, ItemType.EQUIPMENT)
+        {
+            EquipmentType = equipmentType;
+        }
+    }
+}
