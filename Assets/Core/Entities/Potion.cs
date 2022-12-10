2843b11e0e079870a9a8eabf661971aa4c477cee
Lucas <lucasdesenna@gmail.com>
2022-12-10 02:55:55 -0300

    Add Potion Entity

---
+++ Assets/Core/Entities/Potion.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Core.Enums;
+
+namespace Assets.Core.Entities
+{
+    public class Potion : Item
+    {
+        public readonly PotionType PotionType;
+
+        public Potion(string name, string description, PotionType potionType) 
+            : base(name, description, ItemType.POTION)
+        {
+            PotionType = potionType;
+        }
+    }
+}
---
+++ Assets/Core/Enums/PotionType.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Core.Enums
+{
+    public enum PotionType
+    {
+        POTION_OF_HEALING,
+        POTION_OF_STRENGTH,
+        POTION_OF_TOUGHNESS,
+        ELIXIR_OF_SPEED,
+        POTION_OF_INVISIBILITY,
+        EXPLOSIVE_COCKTAIL,
+        POTION_OF_INVULNERABILITY,
+        POTION_OF_TELEPORTATION,
+        DEMON_IN_A_BOTTLE,
+        ELIXIR_OF_LIFE
+    }
+}
