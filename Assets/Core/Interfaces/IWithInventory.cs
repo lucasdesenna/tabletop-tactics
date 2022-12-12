47e2ebb60ee2ab5d923a5773d9b627b67565f4ec
Lucas <lucasdesenna@gmail.com>
2022-12-11 22:47:01 -0300

    Add IWithInventory interface

---
+++ Assets/Core/Interfaces/IWithInventory.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Core.Interfaces
+{
+    public interface IWithInventory
+    {
+        int? ItemCapacity { get; }
+
+        List<IItem> Items { get; }
+
+        bool IsFull { get => ItemCapacity != null && Items.Count() == ItemCapacity; }
+
+        void AddItem(IItem item)
+        {
+            if (IsFull) { throw new InvalidOperationException(); }
+
+            Items.Add(item);
+        }
+
+        void RemoveItem(IItem item) => Items.Remove(item);
+    }
+}
---
+++ Tests/Interfaces/IWithInventoryTest.cs
@@ -0,0 +1,76 @@
+﻿using Assets.Core.Entities;
+using Assets.Core.Interfaces;
+using Moq;
+
+namespace Tests.Interfaces
+{
+    public class IWithInventoryTest
+    {
+        [TestFixture]
+        private class WithInventory : IWithInventory
+        {
+            private int? _itemCapacity = null;
+            public int? ItemCapacity => _itemCapacity;
+
+            private readonly List<IItem> _items = new();
+            public List<IItem> Items => _items;
+
+            public WithInventory() { }
+
+            public WithInventory(int? itemCapacity) {
+                _itemCapacity = itemCapacity;
+            }
+        }
+
+
+        [Test]
+        public void DefaultAddItemTest()
+        {
+            IWithInventory withInvetory = new WithInventory(1);
+
+            Mock<IItem> itemMock = new();
+
+            withInvetory.AddItem(itemMock.Object);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(withInvetory.Items, Contains.Item(itemMock.Object));
+                Assert.That(withInvetory.Items, Has.Count.EqualTo(1));
+                Assert.Throws<InvalidOperationException>(() => withInvetory.AddItem(itemMock.Object));
+            });
+        }
+
+        [Test]
+        public void DefaultAddItemToUnboundInventoryTest()
+        {
+            IWithInventory withInvetory = new WithInventory();
+
+            Mock<IItem> itemMock = new();
+
+            withInvetory.AddItem(itemMock.Object);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(withInvetory.Items, Contains.Item(itemMock.Object));
+                Assert.That(withInvetory.Items, Has.Count.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void DefaultRemoveItemTest()
+        {
+            IWithInventory withInvetory = new WithInventory(1);
+
+            Mock<IItem> itemMock = new();
+            withInvetory.AddItem(itemMock.Object);
+
+            withInvetory.RemoveItem(itemMock.Object);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(withInvetory.Items, !Contains.Item(itemMock.Object));
+                Assert.That(withInvetory.Items, Is.Empty);
+            });
+        }
+    }
+}
