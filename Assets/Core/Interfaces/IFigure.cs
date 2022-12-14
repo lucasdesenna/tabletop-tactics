71f60a87e4df03546357d071c2c28343e3249dc5
Lucas <lucasdesenna@gmail.com>
2022-12-13 21:50:30 -0300

    Transform Figure into an Interface

---
+++ Assets/Core/Interfaces/IFigure.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Core.Enums;
+
+namespace Assets.Core.Interfaces
+{
+    public interface IFigure
+    {
+        public string Name { get; }
+        public Gender Gender {get; }
+        public int Armour { get; }
+        public int Fight { get; }
+        public int Health { get; }
+        public int Move { get; }
+        public int Shoot { get; }
+        public int Will { get; }
+    }
+}
