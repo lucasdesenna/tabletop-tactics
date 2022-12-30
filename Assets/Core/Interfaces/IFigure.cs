2dddadb2f011cfe322c85ec9f46a5ce564bde198
Lucas <lucasdesenna@gmail.com>
2022-12-30 14:08:37 -0300

    Add the IWithAttributes interface

--- Assets/Core/Interfaces/IFigure.cs
+++ Assets/Core/Interfaces/IFigure.cs
@@ -2,31 +2,10 @@
 
 namespace Assets.Core.Interfaces
 {
-    public interface IFigure
+    public interface IFigure : IWithAttributes, IWithTags
     {
-        public string Name { get; }
-        public Gender Gender {get; }
-        public int Armour { get; }
-        public int EffectiveArmour { get; set; }
-        public int Fight { get; }
-        public int EffectiveFight { get; set; }
-        public int MaxHealth { get; }
-        public int Health { get; set; }
-        public int Move { get; }
-        public int EffectiveMove { get; set; }
-        public int Shoot { get; }
-        public int EffectiveShoot { get; set; }
-        public int Will { get; }
-        public int EffectiveWill { get; set; }
-
-        public void ResetAttributes()
-        {
-            EffectiveArmour = Armour;
-            EffectiveFight = Fight;
-            Health = MaxHealth;
-            EffectiveMove = Move;
-            EffectiveShoot = Shoot;
-            EffectiveWill = Will;
-        }
+        string Name { get; }
+        Gender Gender { get; }
+        FigureState State { get; set; } 
     }
 }
---
+++ Assets/Core/Interfaces/IWithAttributes.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Core.Enums;
+
+namespace Assets.Core.Interfaces
+{
+    public interface IWithAttributes
+    {
+        int Armour { get; }
+        int EffectiveArmour { get; set; }
+        int Fight { get; }
+        int EffectiveFight { get; set; }
+        int MaxHealth { get; }
+        int Health { get; set; }
+        int Move { get; }
+        int EffectiveMove { get; set; }
+        int Shoot { get; }
+        int EffectiveShoot { get; set; }
+        int Will { get; }
+        int EffectiveWill { get; set; }
+
+        void ResetAttributes()
+        {
+            EffectiveArmour = Armour;
+            EffectiveFight = Fight;
+            Health = MaxHealth;
+            EffectiveMove = Move;
+            EffectiveShoot = Shoot;
+            EffectiveWill = Will;
+        }
+    }
+}
